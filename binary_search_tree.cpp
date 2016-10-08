c2beab86e4916df3d3db4b5fdba7c06d09d6c9f8
Jesse <scalpel.etf@gmail.com>
2016-10-08 12:51:44 -0500

    using custom stack

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -1,1 +1,58 @@
+//非递归 遍历二叉树的实现
+
 #include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+struct treenode{
+	int payload;
+	treenode *left;
+	treenode *right;
+	treenode(int value):payload(value), left(nullptr), right(nullptr){}
+};
+
+
+
+
+
+void pre_order(treenode *root)	//先序遍历 打印所有节点payload
+{
+	stack<treenode*> s;	//stack s 是用来存储treenode*类型的
+	treenode* current_node = root;
+	while(current_node)
+	{
+		cout << current_node->payload << " ";	//travel this node
+		if(current_node->right)		//先遍历左边, 再遍历右边. 但是因为到左边的leaf后, 很难重新再回到根节点
+			s.push(current_node->right);	//所以把根节点入栈
+
+		if(current_node->left)
+			current_node = current_node->left;	//左边前进
+		else{		//分支遍历完了 要从栈中取出过去存放的通向右边的节点
+			if(s.empty())	//stack head是否为空
+				current_node = nullptr;	//stack head为空, 栈里没有元素, 遍历结束
+		
+			else{		//如果栈不为空
+			current_node = s.top();	//获得栈顶元素
+			s.pop();	//清除栈顶元素
+			}
+		}
+
+	}
+}
+
+
+int main()
+{
+	treenode *root = new treenode(5);
+	root->left = new treenode(3);
+	root->right = new treenode(7);
+	root->left->right = new treenode(4);
+
+	pre_order(root);
+	cout << endl;
+
+	return 0;
+
+
+}
--- stack.h
+++ stack.h
@@ -4,8 +4,8 @@ template <typename T>	//typename 表示可以使用多种数据类型
 struct node{
 	T payload;
 	node* next;
-	node(T payload) {this -> payload = payload;};	//构造函数
-}
+	node (T payload) {this -> payload = payload;};	//构造函数
+};
 //现在node<T>可以用来表示payload类型为T的一个节点类型.
 
 
@@ -25,7 +25,7 @@ public:
 
 	void push(T value)	//压栈
 	{
-		node<T> *new_head = new node<T>(value);
+		node<T> *new_node = new node<T>(value);
 		new_node->next = head;
 		head = new_node;
 	}
@@ -49,5 +49,5 @@ public:
 	}
 
 
-}
+};
 
