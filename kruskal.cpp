356c71a628faee1f2e9bb828493b5dfcd0ec645c
Jesse <scalpel.etf@gmail.com>
2016-11-04 12:42:27 -0500

    minimal spanning tree 最小生成树算法

--- kruskal.cpp
+++ kruskal.cpp
@@ -26,19 +26,50 @@ graph_ad_list kruskal(graph_ad_list &g)
 {	
 	//disjoint_set本质是链表
 	//subsets是一个存放链表的向量
+	//每个节点开始的时候都不是连着的, 每个节点都产生一个并查集
 	vector<disjoint_set *> sub_sets(g.ad_list.size());
 
-	//for each node
+	//for each node 每个节点都有一个独立的并查集
 	for(int i = 0;i<g.ad_list.size();i++)
 		sub_sets[i] = new disjoint_set;
 
-	//边
+	//边 初始化
 	vector<edge> edges;	
 
+	//把所有边聚集在一起,为了后边排序
 	//for each node
 	for(int i = 0; i< g.ad_list.size();i++)
 		//for each edge
 		for(auto pos = g.ad_list[i].begin(); pos != g.ad_list[i].end();pos++)
+		{
+			edge edge(i, pos->dest_id, pos->height);
+			edges.push_back(edge);	//edges是一个edge类型的向量 push_back就是在最后添加
+		}
+
+	//sorting edges by weight
+	//这里用的C++ STL中的排序函数sort(), 最后一个参数是一个自己定义的函数,表示用这个函数来进行排序, 这样可以sort升序或者降序
+	//前边的两个参数表示范围
+	std::sort(edges.begin(), edges.end(), compare_weight);
+
+	//初始化一个存储结果的邻接表
+	graph_ad_list result(g.ad_list.size());
+
+	//从最小的边开始选择路线
+	//for each edge
+	for(int i = 0;i<edges.size();i++)
+	{
+		edge e = edges[i];
+
+	//	if source node and dest node are not in the same sub set
+		if(sub_sets[e.source]->find_root() != sub_sets[e.dest] -> find_root() )
+		{
+			result.add_edge(e.source, e.dest, e.weight);
+			sub_sets[e.source] -> union_set(sub_sets[e.dest]);	//因为subsets是一个指针类型的容器,所以调用元素或者方法要用->
+		}
+	}
+
+	return result;
+
 
 }
 
