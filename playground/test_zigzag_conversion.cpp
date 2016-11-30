fbaee7a0fe8cf9ca0d8493caf78e3f02ecc95cc7
Jesse <scalpel.etf@gmail.com>
2016-11-30 11:14:10 -0600

    debug gcc test

--- playground/test_zigzag_conversion.cpp
+++ playground/test_zigzag_conversion.cpp
@@ -17,23 +17,27 @@
 #include <iostream>
 #include <string>
 
+//如果不需要打印 就注释掉此行
+//#define DEBUG_ME
+
 using namespace std;
 
 string convert(string s, int numRows)
 {
     
-    string *str = new string("");
-    string result = *str;
+    //string *str = new string("");
+    string result = "";
     int n = 0;//n是s的iterator
     int i = 0;//i是result的iterator
     int row = 0;
+    int flag = 0;
     
     
     
     
-    while(i<s.size())
+    while(i<=s.size())
     {
-        if(row == numRows/2)	//中间那行
+        if(numRows%2 == 1 && row == numRows/2)	//写成奇数行时 中间那行
         {
             result[i] = s[n];
             n = n + (numRows+1)/2;
@@ -43,26 +47,53 @@ string convert(string s, int numRows)
                 row++;
             }
         }
+        else if(numRows%2 == 0 && row == numRows/2-1)   //写成偶数行时, 中间那行
+        {
+            if(flag == 0)
+            {
+                flag = 1;
+                result[i] = s[n];
+                n = n + (numRows/2) + 1;
+            }
+            else
+            {
+                flag = 0;
+                result[i] = s[n];
+                n = n + (numRows/2);
+            }
+            
+            if(n > s.size())
+            {
+                n = row + 1;
+                row++;
+            }
+        }
         
         else	//非中间行
         {
             result[i] = s[n];
-            n = n+ (numRows+1) + row;
+            n = n+ (numRows+1);
             if(n>s.size())
             {
                 n = row+1;
                 row++;
             }
         }
-        
+#ifdef DEBUG_ME
         cout << " i = " << i << endl;
         cout << result[i] << endl;
+        
+        cout << result <<endl;
+#endif
+        result = result+result[i];  //???为什么result[i]有值, 而result为空?
         i++;
         
-        
-        
     }
-    cout << "return" <<endl;
+    
+#ifdef DEBUG_ME
+    cout << result <<endl;
+#endif
+    
     return result;
     
     
@@ -70,8 +101,9 @@ string convert(string s, int numRows)
 
 int main ()
 {
-    string source = "PAYPALISHIRING";
-    string output = convert(source,5);
-    cout << output << endl;
+    string source = "AB";
+    string output = convert(source,2);
+    cout << "after convert:" << output << endl;
+   // output.~string();
     return 0;
 }
