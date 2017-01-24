70604c0678fbb37464ece329be1008c12bcb8a5c
Jesse <scalpel.etf@gmail.com>
2017-01-24 13:30:51 -0600

    correct but not accepted

--- playground/test_jump_game.cpp
+++ playground/test_jump_game.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 bool canJump(vector<int>& nums) {
-    int target = nums.size()-1;
+    int target = nums.size();
     int current_max = 0;
-    for(int i = 0; i <= current_max && i<= target; i++)
+    for(int i = 0; i <= current_max && i<= target-1; i++)
     {
         current_max = std::max(nums[i]+i, current_max);
-        if(target <= current_max)
+        if(target <= current_max+1)
             return true;
     }
     return false;
@@ -20,7 +20,8 @@ bool canJump(vector<int>& nums) {
 
 int main()
 {
-    vector<int> test = {1,0,2};
+    //int test2[] = {1,0,2};
+    vector<int> test = {1,0,2};	
     if(canJump(test))
         cout << "True" << endl;
     else cout << "false" << endl;
