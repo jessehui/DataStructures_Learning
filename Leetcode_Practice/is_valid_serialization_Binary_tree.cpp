ad6cfbe4474a69cea70d7e2de4345b083f808701
Jesse <scalpel.etf@gmail.com>
2017-02-22 19:59:54 -0600

    Accepted very slow 5%

--- Leetcode_Practice/is_valid_serialization_Binary_tree.cpp
+++ Leetcode_Practice/is_valid_serialization_Binary_tree.cpp
@@ -38,14 +38,76 @@ Return false
 */
 
 
+
+
+
+
 class Solution {
+private:
+    vector<string> str_to_vec(string preorder)
+    {
+        vector<string> pre_order_str;
+        int index = 0;
+        for(int i = 0; i<preorder.size();i++)
+        {
+            if(preorder[i] == ',')
+            {
+                string temp = preorder.substr(index, 1);
+            //  cout << "temp = " << temp << " ";
+                pre_order_str.push_back(temp);
+                index = i+1;
+            }
+        }
+        string temp = preorder.substr(index,1);
+        pre_order_str.push_back(temp);
+
+        return pre_order_str;
+
+
+        // for(int i = 0; i<pre_order_str.size(); i++)
+        //  cout << pre_order_str[i] << endl;
+    }
+
+
+
 public:
+
     bool isValidSerialization(string preorder) {
-        
+        vector<string> PO_str = str_to_vec(preorder);
+        int size = PO_str.size();
+        while(PO_str.size() > 1)
+        {
+            size = PO_str.size();
+            for(int i = 0; i < size-2; i++)
+            {
+                if(PO_str[i] != "#")
+                {
+                    if(PO_str[i+1] == "#" && PO_str[i+2] == "#")
+                    {
+                        PO_str[i] = "#";
+                        PO_str.erase(PO_str.begin()+i+1, PO_str.begin()+i+3);
+                    }
+                }
+            
+            }
+
+            // for(auto it = PO_str.begin(); it != PO_str.end(); it++)
+            //     cout << *it << " ";
+            // cout << endl;
+            
+
+            if(size == PO_str.size())
+            {
+                return false;
+            }
+        }
+
+        if(PO_str[0] == "#")
+            return true;
+        else
+            return false;
     }
 };
-
-
 
 
 
