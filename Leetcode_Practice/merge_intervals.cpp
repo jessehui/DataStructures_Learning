402ace168269c2c14f395aa290c85c176b7b4187
Jesse <scalpel.etf@gmail.com>
2017-03-15 12:36:31 -0500

    Accepted but very slow 8%

--- Leetcode_Practice/merge_intervals.cpp
+++ Leetcode_Practice/merge_intervals.cpp
@@ -26,37 +26,105 @@ class Solution {
 public:
     vector<Interval> merge(vector<Interval>& intervals) {
         if(intervals.size() <= 1)
-        	return intervals;
+            return intervals;
 
-        for(int i = 1; i < intervals.size(); i++)
-        {           
+        for(int i = 1; i < intervals.size(); )
+        {
+            // if(intervals[i-1].start >= intervals[i].start)
+            // {
+            //     intervals[i-1].start = intervals[i].start;
+            //     intervals.erase(intervals.begin()+i);
+            //     continue;
+            // }
+            
             if(intervals[i].start > intervals[i-1].end || intervals[i].end < intervals[i-1].start)
+            {
+                i++;
                 continue;
+            }
                 
-        	if(intervals[i-1].end >= intervals[i].start)
-        	{
-        	    if(intervals[i].start <= intervals[i-1].start)
-        	    {
-        	        intervals[i-1].start = intervals[i].start;
-        	    }
-        	   
-        	}
-        	
-        	if(intervals[i].end >= intervals[i-1].end)
-        	{
-        	    intervals[i-1].end = intervals[i].end;
-        	    
-        	}
-        	intervals.erase(intervals.begin()+i);
-        	
-        	
-        //	i = i-1;
+            if(intervals[i-1].end >= intervals[i].start)
+            {
+                if(intervals[i].start <= intervals[i-1].start)
+                {
+                    intervals[i-1].start = intervals[i].start;
+                }
+               
+            }
+            
+            if(intervals[i].end >= intervals[i-1].end)
+            {
+                intervals[i-1].end = intervals[i].end;
+                
+            }
+            intervals.erase(intervals.begin()+i);
+            
+            if(intervals[i-1].end < intervals[i].start)
+            {
+                i++;
+            }
+            
+        
         }
         
 
         return intervals;
     }
 };
+
+
+//another try
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+bool compare(Interval itv1, Interval itv2)
+{
+    return itv1.start<itv2.start;
+}
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        if(intervals.size() <= 1)
+            return intervals;
+        sort(intervals.begin(), intervals.end(), compare);
+
+        int min = intervals[0].start;
+        int max = intervals[0].end;
+        for(int i = 1; i<intervals.size(); i++)
+        {
+            if(intervals[i].start > max)
+            {
+                min = intervals[i].start;
+                max = intervals[i].end;
+                continue;
+            }  
+              
+            if(intervals[i].start < max)
+            {
+                if(intervals[i].end > max)
+                {
+                    max = intervals[i].end;
+                }
+            }
+        }
+
+    }
+};
+
+
+
+
+
+
+
+
 
 
 
